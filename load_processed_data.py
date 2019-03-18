430da044f305054d28e4a4e3e27a9e834313dbd4
starkof <starkof>
2019-03-18 22:35:35 +0000

    Added code to load pre-processed data into mongodb

---
+++ load_processed_data.py
@@ -0,0 +1,39 @@
+import json
+import gzip
+
+# import mongoengine as mongo
+from pymongo import MongoClient
+
+
+client = MongoClient('localhost', 27017)
+db = client['amazon_books']
+database = db['dummy_users']
+
+
+with open('data/user_data.json') as f:
+    n = 0
+    for user in f:
+
+        if n % 1000 == 0:
+            print(user)
+
+        data = json.loads(user)
+
+        try:
+            del data['_id']
+        except KeyError:
+            pass
+
+        # print('books', data['books'][0]['unix_review_time']['$numberInt'])
+
+        for book in data['books']:
+
+            book['rating'] = int(book['rating']['$numberInt'])
+            book['unix_review_time'] = int(book['unix_review_time']['$numberInt'])
+
+        database.insert(data)
+
+        n += 1
+
+
+client.close()
